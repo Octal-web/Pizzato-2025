ea83bbebe3febca026344967c8a2dcafb598ce54
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-07-13 09:56:22 -0300

    feat: adiciona ao menus a página com as perguntas requentes

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,28 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import { Link, Head, usePage } from '@inertiajs/react';
+import React, { useState, useEffect } from "react";
+import { Link, Head, usePage } from "@inertiajs/react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faWineBottle, faBars, faTimes, faPowerOff, faSignOut, faInfoCircle, faEnvelope, faFileText, faWineGlass, faPortrait } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faHome,
+    faWineBottle,
+    faBars,
+    faTimes,
+    faPowerOff,
+    faSignOut,
+    faInfoCircle,
+    faEnvelope,
+    faFileText,
+    faWineGlass,
+    faPortrait,
+    faCircleQuestion,
+} from "@fortawesome/free-solid-svg-icons";
 
-import { MenuItem } from '@/Components/Manager/MenuItem';
-import { ConfirmModal } from '@/Components/Manager/ConfirmModal';
-import { NotificationMessage } from '@/Components/Manager/NotificationMessage';
+import { MenuItem } from "@/Components/Manager/MenuItem";
+import { ConfirmModal } from "@/Components/Manager/ConfirmModal";
+import { NotificationMessage } from "@/Components/Manager/NotificationMessage";
 
 const menus = [
-   
-    { id: 1, label: 'Home', icon: faHome, href: route('Manager.Home.index'), controllers: ['Home', 'Slides', 'Clientes'] },
-    { id: 2, label: 'Institucional', icon: faInfoCircle, controllers: ['Institucional', 'Vinhedos', 'Acontecimentos'], subMenu: [
-        { label: 'Institucional', href: route('Manager.Institucional.index') },
-        { label: 'História', href: route('Manager.Institucional.historia') },
-        { label: 'Sustentabilidade', href: route('Manager.Institucional.sustentabilidade') }
-    ]},
-    { id: 3, label: 'Produtos', icon: faWineBottle, controllers: ['Linhas', 'Categorias', 'Produtos'], subMenu: [
-        { label: 'Linhas', href: route('Manager.Linhas.index') },
-        { label: 'Produtos', href: route('Manager.Produtos.index') },
-    ]},
-    { id: 4, label: 'Enoturismo', icon: faWineGlass, href: route('Manager.Enoturismo.index'), controllers: ['Enoturismo', 'Experiencias']},
-    { id: 5, label: 'Contato', icon: faEnvelope, href: route('Manager.Contato.index'), controllers: ['Contato', 'Newsletter'] },
-    { id: 6, label: 'Política de Privacidade', icon: faFileText, href: route('Manager.Politicas.privacidade'), controllers: ['Politicas'] }
+    {
+        id: 1,
+        label: "Home",
+        icon: faHome,
+        href: route("Manager.Home.index"),
+        controllers: ["Home", "Slides", "Clientes"],
+    },
+    {
+        id: 2,
+        label: "Institucional",
+        icon: faInfoCircle,
+        controllers: ["Institucional", "Vinhedos", "Acontecimentos"],
+        subMenu: [
+            {
+                label: "Institucional",
+                href: route("Manager.Institucional.index"),
+            },
+            {
+                label: "História",
+                href: route("Manager.Institucional.historia"),
+            },
+            {
+                label: "Sustentabilidade",
+                href: route("Manager.Institucional.sustentabilidade"),
+            },
+        ],
+    },
+    {
+        id: 3,
+        label: "Produtos",
+        icon: faWineBottle,
+        controllers: ["Linhas", "Categorias", "Produtos"],
+        subMenu: [
+            { label: "Linhas", href: route("Manager.Linhas.index") },
+            { label: "Produtos", href: route("Manager.Produtos.index") },
+        ],
+    },
+    {
+        id: 4,
+        label: "Enoturismo",
+        icon: faWineGlass,
+        href: route("Manager.Enoturismo.index"),
+        controllers: ["Enoturismo", "Experiencias"],
+    },
+    {
+        id: 5,
+        label: "Contato",
+        icon: faEnvelope,
+        href: route("Manager.Contato.index"),
+        controllers: ["Contato", "Newsletter"],
+    },
+    {
+        id: 6,
+        label: "Perguntas frequentes",
+        icon: faCircleQuestion,
+        href: route("Manager.Perguntas.index"),
+        controllers: "Perguntas",
+    },
+    {
+        id: 7,
+        label: "Política de Privacidade",
+        icon: faFileText,
+        href: route("Manager.Politicas.privacidade"),
+        controllers: ["Politicas"],
+    },
 ];
 
 const AdminEnologiaLayout = ({ children }) => {
@@ -45,12 +109,14 @@ const AdminEnologiaLayout = ({ children }) => {
         setIsModalOpen(true);
     };
 
-      const closeModal = () => {
+    const closeModal = () => {
         setIsModalOpen(false);
     };
 
     useEffect(() => {
-        const activeMenu = menus.find(menu => menu.controllers.includes(controller));
+        const activeMenu = menus.find((menu) =>
+            menu.controllers.includes(controller),
+        );
         if (activeMenu) {
             setOpenMenuId(activeMenu.id);
         }
@@ -59,11 +125,17 @@ const AdminEnologiaLayout = ({ children }) => {
     useEffect(() => {
         if (message) {
             const newNotification = { id: Date.now(), ...message };
-            setNotifications((prevNotifications) => [...prevNotifications, newNotification]);
+            setNotifications((prevNotifications) => [
+                ...prevNotifications,
+                newNotification,
+            ]);
 
             const timer = setTimeout(() => {
                 setNotifications((prevNotifications) =>
-                    prevNotifications.filter((notification) => notification.id !== newNotification.id)
+                    prevNotifications.filter(
+                        (notification) =>
+                            notification.id !== newNotification.id,
+                    ),
                 );
             }, 5300);
 
@@ -80,25 +152,49 @@ const AdminEnologiaLayout = ({ children }) => {
             <header></header>
             <div className="flex bg-slate-50">
                 <div className="fixed top-0 left-0 w-full bg-black z-50 px-4 py-3 flex justify-between items-center md:hidden">
-                    <Link href={route('Manager.Home.index')}>
+                    <Link href={route("Manager.Home.index")}>
                         <div className="company-logo w-8 h-10 bg-no-repeat bg-center bg-contain"></div>
                     </Link>
-                    <button onClick={toggleMobileMenu} className="text-white p-2">
-                        <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} className="text-xl" />
+                    <button
+                        onClick={toggleMobileMenu}
+                        className="text-white p-2"
+                    >
+                        <FontAwesomeIcon
+                            icon={isMobileMenuOpen ? faTimes : faBars}
+                            className="text-xl"
+                        />
                     </button>
                 </div>
 
-                <aside className={`menu-aside fixed md:absolute left-0 top-0 z-[2] flex h-full md:h-[calc(100vh-7rem)] w-full md:w-16 md:hover:w-64 flex-col overflow-hidden bg-black md:rounded-xl duration-200 ease-linear lg:fixed lg:translate-x-0 -translate-x-full md:mx-10 md:my-14 shadow-lg${isMobileMenuOpen ? ' translate-x-0' : ''}`}>
+                <aside
+                    className={`menu-aside fixed md:absolute left-0 top-0 z-[2] flex h-full md:h-[calc(100vh-7rem)] w-full md:w-16 md:hover:w-64 flex-col overflow-hidden bg-black md:rounded-xl duration-200 ease-linear lg:fixed lg:translate-x-0 -translate-x-full md:mx-10 md:my-14 shadow-lg${isMobileMenuOpen ? " translate-x-0" : ""}`}
+                >
                     <div className="w-64">
                         <div className="flex items-center justify-between gap-2 px-4">
-                            <Link href={route('Manager.Home.index')} className="mt-6">
-                                <div className="company-logo w-8 h-16 bg-no-repeat bg-top-center bg-contain transition-all hidden md:block">
-                                </div>
+                            <Link
+                                href={route("Manager.Home.index")}
+                                className="mt-6"
+                            >
+                                <div className="company-logo w-8 h-16 bg-no-repeat bg-top-center bg-contain transition-all hidden md:block"></div>
                             </Link>
 
-                            <button aria-controls="sidebar" aria-expanded="false" className="block lg:hidden text-white">
-                                <svg className="fill-current" width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z" fill=""></path>
+                            <button
+                                aria-controls="sidebar"
+                                aria-expanded="false"
+                                className="block lg:hidden text-white"
+                            >
+                                <svg
+                                    className="fill-current"
+                                    width="20"
+                                    height="18"
+                                    viewBox="0 0 20 18"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
+                                        fill=""
+                                    ></path>
                                 </svg>
                             </button>
                         </div>
@@ -126,8 +222,14 @@ const AdminEnologiaLayout = ({ children }) => {
                             </nav>
 
                             <div className="absolute bottom-0 left-0">
-                                <button className="text-sm ml-3 mb-3 p-3" onClick={openModal}>
-                                    <FontAwesomeIcon icon={faPowerOff} className="text-lg text-white" />
+                                <button
+                                    className="text-sm ml-3 mb-3 p-3"
+                                    onClick={openModal}
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faPowerOff}
+                                        className="text-lg text-white"
+                                    />
                                 </button>
                             </div>
                         </div>
@@ -140,9 +242,15 @@ const AdminEnologiaLayout = ({ children }) => {
                     </div>
                 </div>
             </div>
-            
-            {isModalOpen && <ConfirmModal icon={faSignOut} closeModal={closeModal} type="logOut" confirm={route('Manager.Usuarios.logout')} />}
 
+            {isModalOpen && (
+                <ConfirmModal
+                    icon={faSignOut}
+                    closeModal={closeModal}
+                    type="logOut"
+                    confirm={route("Manager.Usuarios.logout")}
+                />
+            )}
 
             {notifications.map((notification, key) => (
                 <NotificationMessage
@@ -152,7 +260,9 @@ const AdminEnologiaLayout = ({ children }) => {
                     show={true}
                     onClose={() =>
                         setNotifications((prevNotifications) =>
-                            prevNotifications.filter((n) => n.id !== notification.id)
+                            prevNotifications.filter(
+                                (n) => n.id !== notification.id,
+                            ),
                         )
                     }
                 />
